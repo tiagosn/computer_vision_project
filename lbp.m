52bb1bcf94f07a61052dc921e1a6c3eeb9f37589
Tiago <tiagosn92@gmail.com>
2015-05-26 18:49:09 -0300

    LBP without histogram

--- lbp.m
+++ lbp.m
@@ -1,8 +1,10 @@
 % baseado no codigo de: http://www.bytefish.de/blog/local_binary_patterns/
 
 function out = lbp(src, P, R)
-    U = zeros(size(src), 'uint8');
-    out = zeros(size(src), 'uint8');
+    u = zeros(size(src));
+    %out = zeros(size(src), 'uint8');
+    out = zeros(size(src));
+    code = zeros(size(src));
     
     rows = size(src, 1);
     cols = size(src, 2);
@@ -28,14 +30,24 @@ function out = lbp(src, P, R)
             for j = (R+1):(cols-(R+1))
                 aux = w1*src(i+fx,j+fy) + w2*src(i+cx,j+fy) + w3*src(i+fx,j+cy) + w4*src(i+cx,j+cy);
                 if(aux >= src(i,j))
-                    out(i,j) = out(i,j) + 2^(p-1);
+                    code(i,j) = code(i,j) + 2^(p-1);
+                    out(i,j) = out(i,j) + 1;
                 end
             end
         end
     end
     
+    p1 = bitget(out, P);
+    p2 = bitget(out, 1);
+    u = u + xor(p1, p2);
+    for p = 1:P
+        p1 = p2;
+        p2 = bitget(out, p);
+        u = u + xor(p1, p2);
+    end
+    
+    out(u>2) = P+1;
+    
     % TODO:
-    %   - fazer shifts para calcular a uniformidade de out
-    %   - colocar os valores dos pontos uniformes e não uniformes em out
     %   - calcular o histograma 
 end
